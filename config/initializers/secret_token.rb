ac280fa31e7ee35f2aa4137df4e23b8e5f0744c1
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2013-02-25 10:20:37 -0300

    Rails Application Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powder::Application.config.secret_token = '5b91802f7557b0f168ed9b15939c7dbb65e37b997127f63064d97c81b0127bb4916ccdb8db3a45be1d9f28a11b0834c7be3f6790fa4be201f32517c585a57b13'
